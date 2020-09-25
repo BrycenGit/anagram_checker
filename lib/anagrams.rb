a5230b8d1d009600e51121b9fb4ab822c3335b81
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-25 09:03:00 -0700

    change test to have 2 inputs, fail test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,12 +1,13 @@
 class Anagram
   attr_accessor (:phrase)
 
-  def initialize(phrase)
+  def initialize(phrase, phrase2)
     @phrase = phrase
+    @phrase2 = phrase2
   end
 
   def has_vowel
-    @phrase.match?(/[aeiou]/i)
+    @phrase.match?(/[aeiou]/i) && @phrase2.match?(/[aeiou]/i)
   end
 
   def split_phrase
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,13 +2,13 @@ require ('rspec')
 require ('anagrams')
 
 describe ('Anagram#anagrams') do
-  it ('will check the input to see if it contains vowels') do
-    anagram = Anagram.new('hello')
-    expect(anagram.has_vowel).to(eq(true))
+  it ('will check both inputs to see if they contain vowels') do
+    anagram = Anagram.new('hello', 'good')
+    expect(anagram.has_vowel).to(eq(false))
   end
 
-  it ('will split phrase into an array words') do
-    anagram = Anagram.new('hello my friend!')
-    expect(anagram.split_phrase).to(eq(["hello", "my", "friend"]))
-  end
+  # it ('will split phrase into an array words and will remove numbers and characters') do
+  #   anagram = Anagram.new('hello my friend!')
+  #   expect(anagram.split_phrase).to(eq(["hello", "my", "friend"]))
+  # end
 end
