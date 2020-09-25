4c23897823dbd7979fba7d4ec50cf3713d3ce6e1
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-25 08:38:00 -0700

    pass test, will check the input to see if it contains vowels

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,6 +4,6 @@ require ('anagrams')
 describe ('Anagram#anagrams') do
   it ('will check the input to see if it contains vowels') do
     anagram = Anagram.new('hello')
-    expect(anagram.has_vowel).to(eq(false))
+    expect(anagram.has_vowel).to(eq(true))
   end
 end
