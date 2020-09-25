fd21a8682f78462b30ea4381fa630a6523c88fcc
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-25 08:43:06 -0700

    fail test, will split phrase into an array words

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,4 +6,8 @@ describe ('Anagram#anagrams') do
     anagram = Anagram.new('hello')
     expect(anagram.has_vowel).to(eq(true))
   end
+
+  it ('will split phrase into an array words') do
+    anagram = Anagram.new('hello my friend!')
+    expect(anagram.split_words).to(eq([hello]))
 end
