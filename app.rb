112b28084491adc2de05fa1f63bbfb0dfcbd770b
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-25 11:34:15 -0700

    add content to app.rb

---
+++ app.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require './lib/anagrams.rb'
+
+puts '----------------------------'
+puts 'Enter a word or phrase'
+puts '----------------------------'
+input = gets.chomp
+puts '----------------------------'
+puts 'Enter another word or phrase'
+puts '----------------------------'
+input2 = gets.chomp
+anagram = Anagram.new(input, input2)
+
+puts '----------------------------'
+puts anagram.anagram_check
+puts '----------------------------'
