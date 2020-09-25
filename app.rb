6ed62c87578e14b1e98a240d6a18477772a50efb
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-25 13:25:50 -0700

    reading terminal documentation

--- app.rb
+++ app.rb
@@ -3,15 +3,11 @@ require './lib/anagrams.rb'
 
 puts '----------------------------'
 puts 'Enter a word or phrase'
-puts '----------------------------'
 input = gets.chomp
 puts '----------------------------'
 puts 'Enter another word or phrase'
-puts '----------------------------'
 input2 = gets.chomp
+puts '----------------------------'
 anagram = Anagram.new(input, input2)
-
-puts '----------------------------'
 puts anagram.anagram_check
-puts '----------------------------'
 
